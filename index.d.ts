bf648a7219bb44ab45192a2ed9b018acec097d22
Maximilian Richter <richter.maximilian@gmx.net>
2022-07-02 15:31:46 +0200

    fix fn signature

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,19 @@
 // Type definitions
 
-export function read(url: string, requestFn: (getRequestOptions: () => any) => { data: any, headers: any }): Promise<FeedData>;
-
+export function read(
+  url: string,
+  requestFn: (
+    url: string,
+    getRequestOptions: () => any
+  ) => { data: any; headers: any }
+): Promise<FeedData>;
 
 export interface FeedData {
-    link?: string;
-    title?: string;
-    description?: string;
-    generator?: string;
-    language?: string;
-    published?: string;
-    entries?: Array<any>;
+  link?: string;
+  title?: string;
+  description?: string;
+  generator?: string;
+  language?: string;
+  published?: string;
+  entries?: Array<any>;
 }
