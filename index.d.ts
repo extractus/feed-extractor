af399a6b86884c6f9849faafd74d509e5d42908e
Dong Nguyen <ndaidong@gmail.com>
2022-09-20 12:52:55 +0700

    v6.1.0rc3
    - Update type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
 // Type definitions
 
-export function read(url: string, options?: object): Promise<FeedData>;
-
+export interface FeedEntry {
+    link?: string;
+    title?: string;
+    description?: string;
+    published?: Date;
+}
 
 export interface FeedData {
     link?: string;
@@ -9,6 +13,36 @@ export interface FeedData {
     description?: string;
     generator?: string;
     language?: string;
-    published?: date;
-    entries?: array;
+    published?: Date;
+    entries?: Array<FeedEntry>;
 }
+
+export interface ReaderOptions {
+  /**
+   * normalize feed data or keep original
+   * default: true
+   */
+  normalization?: Boolean;
+  /**
+   * include full content of feed entry if present
+   * default: false
+   */
+   includeEntryContent?: Boolean;
+  /**
+   * include optional elements if any
+   * default: false
+   */
+   includeOptionalElements?: Boolean;
+  /**
+   * convert datetime to ISO format
+   * default: true
+   */
+   useISODateFormat?: Boolean;
+  /**
+   * to truncate description
+   * default: 210
+   */
+   descriptionMaxLen?: number;
+}
+
+export function read(url: string, options?: ReaderOptions): Promise<FeedData>;
